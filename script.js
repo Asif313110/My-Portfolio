15df5aa508747bc6837d597b12c86bb8694c3eda
Asif Hussain <asifhussainbalti110@gmail.com>
2025-01-22 05:09:15 +0500

    my portfolio

---
+++ script.js
@@ -0,0 +1,8 @@
+// typing animation
+
+var typed = new Typed(".typing",{
+    strings:["","Web Designer","web Developer","Greaphic Designer","Wordpress Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+} )
